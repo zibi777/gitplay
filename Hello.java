4f8dd02b06c95f03d26d03dc72eb516948555f6d
zibi777 <zkruczynski@gmail.com>
2017-12-16 13:45:51 +0100

    asdf 2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello {
 	public static void main (String[] args) {
 	System.out.println("Hello yyy");
-		System.out.println("asdfasdf");
+    System.out.println("asdfasdf");
+    System.out.println("asdfasdf   2");
 	}
 }
